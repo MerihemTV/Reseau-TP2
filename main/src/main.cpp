8a59bc24d9ee18b0325146ed1b782f56473e85ee
LadyCookie <bouzerea@etu.insa-toulouse.fr>
2019-10-22 15:20:43 -0400

    main v2, all argument placed, must remove client code

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -3,24 +3,6 @@
 #include <iostream>
 #include <server.hpp>
 
-std::vector< std::shared_ptr<uvw::TCPHandle>> clients;
-
-void listen(uvw::Loop& loop) {
-	std::shared_ptr<uvw::TCPHandle> tcp = loop.resource<uvw::TCPHandle>();
-
-	tcp->on<uvw::ListenEvent>([tcp](const uvw::ListenEvent&, uvw::TCPHandle& srv) {
-		std::cout << "New Connection" << std::endl;
-		std::shared_ptr<uvw::TCPHandle> client = srv.loop().resource<uvw::TCPHandle>();
-
-		client->on<uvw::CloseEvent>([ptr = srv.shared_from_this()](const uvw::CloseEvent&, uvw::TCPHandle&) { ptr->close(); });
-		client->on<uvw::EndEvent>([](const uvw::EndEvent&, uvw::TCPHandle& client) { client.close(); });
-
-		srv.accept(*client);
-	});
-	tcp->bind("127.0.0.1", 4242);
-	tcp->listen();
-}
-
 void conn(uvw::Loop& loop) {
 	auto tcp = loop.resource<uvw::TCPHandle>();
 
@@ -37,10 +19,13 @@ void conn(uvw::Loop& loop) {
 }
 
 int main(int argc, char* argv[]) {
-	//If this is a server
 	std::string status = argv[1];
+	std::string addr = argv[2];
+	std::string port = argv[3];
+
+	//If programme executed as server
 	if (status == "server") {
-		Server srv("127.0.0.1", 4242);
+		Server srv(addr, atoi(port.c_str()));
 	}
 	else if (status == "client") {
 		auto loop = uvw::Loop::getDefault();
