bb94ba940e43f00504907b37bdc34740da019432
LadyCookie <bouzerea@etu.insa-toulouse.fr>
2019-10-24 14:15:56 -0400

    basic server in class OK

--- main/include/server.hpp
+++ main/include/server.hpp
@@ -10,10 +10,12 @@ class Server
 {
 public:
 	Server(std::string addr, int port);
-	void Send(uint8_t* data, int data_size);
+	void listenServer(uvw::Loop& loop);
+	//Server(std::string addr, int port);
+	//void Send(uint8_t* data, int data_size);
 private:
 	std::string m_addr;
 	int m_port;
-	void listen(uvw::Loop& loop);
-	std::vector< std::shared_ptr<uvw::TCPHandle>> clients;
+	//void listen(uvw::Loop& loop);
+	//std::vector< std::shared_ptr<uvw::TCPHandle>> clients;
 };
--- main/src/main.cpp
+++ main/src/main.cpp
@@ -6,16 +6,42 @@
 #include "player.hpp"
 #include "enemy.hpp"
 
+#include <iostream>
+
+#include <uvw.hpp>
+#include <memory>
+
+void listen(uvw::Loop& loop) {
+	std::shared_ptr<uvw::TCPHandle> tcp = loop.resource<uvw::TCPHandle>();
+
+	tcp->once<uvw::ListenEvent>([](const uvw::ListenEvent&, uvw::TCPHandle& srv) {
+		std::shared_ptr<uvw::TCPHandle> client = srv.loop().resource<uvw::TCPHandle>();
+		std::cout << "New connection" << std::endl;
+
+		client->on<uvw::CloseEvent>([ptr = srv.shared_from_this()](const uvw::CloseEvent&, uvw::TCPHandle&) { ptr->close(); });
+		client->on<uvw::EndEvent>([](const uvw::EndEvent&, uvw::TCPHandle& client) { client.close(); });
+		client->on<uvw::DataEvent>([](const uvw::DataEvent&, uvw::TCPHandle&) {std::cout << "Data received" << std::endl; });
+		srv.accept(*client);
+		client->read();
+	});
+
+	tcp->bind("127.0.0.1", 4242);
+	tcp->listen();
+}
+
 void conn(uvw::Loop& loop) {
 	auto tcp = loop.resource<uvw::TCPHandle>();
 
 	tcp->on<uvw::ErrorEvent>([](const uvw::ErrorEvent&, uvw::TCPHandle&) { /* handle errors */ });
 
-	tcp->once<uvw::ConnectEvent>([](const uvw::ConnectEvent&, uvw::TCPHandle& tcp) {
-		
-	auto dataWrite = std::unique_ptr<char[]>(new char[2]{ 'b', 'c' });
-	tcp.write(std::move(dataWrite), 2);
-	tcp.close();
+	tcp->on<uvw::ConnectEvent>([](const uvw::ConnectEvent&, uvw::TCPHandle& tcp) {
+		auto dataWrite = std::unique_ptr<char[]>(new char[2]{ 'b', 'c' });
+		tcp.write(std::move(dataWrite), 2);
+		tcp.close();
+	});
+
+	tcp->on<uvw::DataEvent>([](const uvw::DataEvent& evt, uvw::TCPHandle&) {
+		std::cout << evt.data.get() << std::endl;
 	});
 
 	tcp->connect(std::string{ "127.0.0.1" }, 4242);
@@ -23,16 +49,46 @@ void conn(uvw::Loop& loop) {
 
 
 int main(int argc, char* argv[]) {
-	classRegistry& CR = classRegistry::getInstance();
-	CR.saveClassInRegistry<Player>();
-	/*
+
+	//args 
+	std::string status = argv[1];
+	std::string addr = argv[2];
+	std::string port = argv[3];
+
+	
+	if (status == "server") {
+		auto srv = Server("127.0.0.1",4242);
+		
+		/*
+		auto loop = uvw::Loop::getDefault();
+		listen(*loop);
+		loop->run();
+		*/
+		
+	}
+	else if (status == "client") {
+		auto loop = uvw::Loop::getDefault();
+		conn(*loop);
+		loop->run();
+	}
+	else {
+		std::cout << "Use: 0 for server, 1 for client" << std::endl;
+	}
+	return 0;
+}
+/*
+int main(int argc, char* argv[]) {
+	
 	std::string status = argv[1];
 	std::string addr = argv[2];
 	std::string port = argv[3];
 
 	//If programme executed as server
 	if (status == "server") {
-		Server srv(addr, atoi(port.c_str()));
+		//Server srv(addr, atoi(port.c_str()));
+		auto loop = uvw::Loop::getDefault();
+		listen(*loop);
+		loop->run();
 	}
 	else if (status == "client") {
 		auto loop = uvw::Loop::getDefault();
@@ -42,6 +98,5 @@ int main(int argc, char* argv[]) {
 	else {
 		std::cout << "Use: 0 for server, 1 for client" << std::endl;
 	}
-	*/
 	return 0;
-}
+}*/
